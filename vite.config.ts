ef14a93ad92d3d48bbe41221edacd9fd73624fd7
fdeleflie <fdeleflie@gmail.com>
2026-04-17 09:52:37 +0200

    avec le bon fiche de config vite ..

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/toilettage-pro---smart-grooming-management/',
+      base: '/kanine/',
       server: {
         port: 3000,
         host: '0.0.0.0',
